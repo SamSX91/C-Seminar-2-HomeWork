2c96d6cb1f6e7496139989d3e2bb0621477dac9d
SamSX91 <128014851+SamSX91@users.noreply.github.com>
2023-04-16 16:16:56 +0700

    Add files via upload

---
+++ Задача 10.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string DigitNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Вторая цифра введенного числа: " + DigitNumber [1]);
---
+++ Задача 15.cs
@@ -0,0 +1,24 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите цифру дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckDay (int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день выходой!");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Введенная цифра не является днем недели:(");
+    }
+    else Console.WriteLine("Этот день не выходной.");
+}
+
+CheckDay(dayNumber);
